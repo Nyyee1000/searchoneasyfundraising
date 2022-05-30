8c4d5bfff1522ce1eb636c0017bd0952f94a0c5b
Nyyee1000 <91501619+Nyyee1000@users.noreply.github.com>
2022-05-30 22:07:53 +0100

    Add files via upload

---
+++ searchingforaclause.java
@@ -0,0 +1,17 @@
+package search;
+
+import io.cucumber.java.en.Given;
+
+
+public class searchingforaclause {
+	
+	@Given("A message with {string} should be displayed")
+	public void a_message_with_should_be_displayed(String string) {
+	    System.out.println("sky");
+	}
+	
+	    }
+	    
+	    
+
+
